b05e5ca287eca7be28fa62b4b8840295863885df
Lemich Maksym <lemichello.m@gmail.com>
2019-12-31 20:16:10 +0200

    refactor(config): changed config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api'
+      : 'https://products-search-api.herokuapp.com/api'
 };
