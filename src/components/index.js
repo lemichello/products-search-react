8b90cd8b74dd97b63a3611f99d17ca776c1cfbd6
Lemich Maksym <lemichello.m@gmail.com>
2020-01-01 00:13:28 +0200

    feat(*): added loading spinner

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { FilterableProductTable } from './FilterableProductTable';
+import axios from 'axios';
+import { Spinner } from '@blueprintjs/core';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  axios.interceptors.request.use(request => {
+    setIsLoading(true);
+    return request;
+  });
+
+  axios.interceptors.response.use(response => {
+    setIsLoading(false);
+    return response;
+  });
+
   return (
     <div className={'app'}>
       <FilterableProductTable />
+      <div>
+        {isLoading && (
+          <Spinner className={'spinner'} size={Spinner.SIZE_LARGE} />
+        )}
+      </div>
     </div>
   );
 }
