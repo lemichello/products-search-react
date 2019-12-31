13bd8a716e8d2045b1a356ae6cb8dba39c0b6c5c
Lemich Maksym <lemichello.m@gmail.com>
2019-12-31 14:51:36 +0200

    Updates

---
+++ precache-manifest.4efa1d6616a3a5dbca6504fa1521ff30.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48757497edabb0e9588876f95d43733e",
+    "url": "/products-search-react/index.html"
+  },
+  {
+    "revision": "b8c2e70b63055955323d",
+    "url": "/products-search-react/static/css/2.79ed69b9.chunk.css"
+  },
+  {
+    "revision": "461f7991497f2e870008",
+    "url": "/products-search-react/static/css/main.b1c0798e.chunk.css"
+  },
+  {
+    "revision": "b8c2e70b63055955323d",
+    "url": "/products-search-react/static/js/2.3dd77000.chunk.js"
+  },
+  {
+    "revision": "a37b9140065d2cd01fbfd733b928e433",
+    "url": "/products-search-react/static/js/2.3dd77000.chunk.js.LICENSE"
+  },
+  {
+    "revision": "461f7991497f2e870008",
+    "url": "/products-search-react/static/js/main.ca63eff4.chunk.js"
+  },
+  {
+    "revision": "5aceb1fcf4a31f9abea9",
+    "url": "/products-search-react/static/js/runtime-main.7db95e7e.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/products-search-react/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/products-search-react/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/products-search-react/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/products-search-react/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/products-search-react/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/products-search-react/static/media/icons-20.ca7934b3.eot"
+  }
+]);
